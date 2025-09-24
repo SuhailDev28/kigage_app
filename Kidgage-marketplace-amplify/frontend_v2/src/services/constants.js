efc7abbe20e5016c65c057eafa41b29b60f49981
SuhailDev28 <156823787+SuhailDev28@users.noreply.github.com>
2025-09-24 15:01:45 +0300

    Update constants.js

--- Kidgage-marketplace-amplify/frontend_v2/src/services/constants.js
+++ Kidgage-marketplace-amplify/frontend_v2/src/services/constants.js
@@ -1,1 +1,2 @@
-export const server_Url = "https://www.kidgage.com/api"; // "http://localhost:5000/api";
+//export const server_Url = "https://www.kidgage.com/api"; // "http://localhost:5000/api";
+export const server_Url = "https://kigage-app-backend.onrender.com/";
